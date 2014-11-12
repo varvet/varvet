7955a2c1c6d76eeedd1b90a4350fc243fa8c1ad8
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-11-12 12:37:55 +0100

    Adds binary and prepared template

--- lib/varvet.rb
+++ lib/varvet.rb
@@ -1,6 +1,24 @@
-require "unicorn"
 require "seedbank"
+require "thor"
+require "unicorn"
 require "varvet/version"
 
 module Varvet
+  class Command < Thor
+    desc "new", "Creates a new Rails project using the Varvet template"
+    def new(name)
+      system("rails new #{name} --database=mysql --template=#{template_path}")
+    end
+
+    desc "update", "Updates the Rails project with the latest Varvet template"
+    def update
+      system("rake rails:template LOCATION=#{template_path}")
+    end
+
+    private
+
+    def template_path
+      File.expand_path("../template.rb", File.dirname(__FILE__))
+    end
+  end
 end
---
+++ template.rb
@@ -0,0 +1,1 @@
+say_status "varvet", "Booting main frame\n", :yellow
