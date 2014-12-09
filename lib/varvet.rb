f7788e318393843c55795f26ddb494fad5317944
Mattias Warnqvist <mwq@mwq.se>
2014-12-09 10:14:48 +0100

    Remove capistrano and postdeploy

--- lib/varvet.rb
+++ lib/varvet.rb
@@ -5,17 +5,6 @@ require "varvet/version"
 require "yaml"
 
 module Varvet
-  class VarvetConfig
-    def self.config()
-      defaults = YAML.load_file("#{VarvetConfig.gem_root}/config/default_config.yml")
-      local_config = YAML.load_file("config/varvet.yml")
-      defaults.merge(local_config)
-    end
-
-    def self.gem_root
-      "#{File.dirname(__FILE__)}/.."
-    end
-  end
 
   class Command < Thor
     include Thor::Actions
@@ -35,37 +24,5 @@ module Varvet
       system("bundle exec cap #{env} deploy")
     end
 
-    desc "postdeploy ENV", "Runs on servers after deploy"
-    def postdeploy(env)
-      Command.source_root("#{gem_root}/deploy_files")
-      @config =  VarvetConfig.config
-      files = ['Procfile','.foreman','config/unicorn.rb','unicorn_launcher','config/database.yml']
-      files.each do |f|
-        template(f)
-      end
-      files = ['config/database.yml']
-      files.each do |f|
-        copy_file(f)
-      end
-      chmod 'unicorn_launcher', 0755
-      create_file '.env', "RAILS_ENV=#{env}\n"
-      service_dir = File.join(ENV['HOME'],'service')
-      run "foreman export runit #{service_dir}"
-
-    end
-
-    private
-
-    def fetch(key)
-      @config.fetch(key.to_s)
-    end
-
-    def gem_root
-      Varvet::VarvetConfig.gem_root
-    end
-
-    def template_path
-      File.expand_path("../template.rb", File.dirname(__FILE__))
-    end
   end
 end
