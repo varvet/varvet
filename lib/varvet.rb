6e0c718b925a1c0e5c62bd8fde3abfa4fd63fb75
Mattias Warnqvist <mwq@mwq.se>
2014-12-16 13:01:49 +0100

    Move command to separate file. Load rails_stdout_logging

--- lib/varvet.rb
+++ lib/varvet.rb
@@ -1,28 +1,4 @@
 require "seedbank"
-require "thor"
 require "unicorn"
-require "varvet/version"
-require "yaml"
+require "rails_stdout_logging"
 
-module Varvet
-
-  class Command < Thor
-    include Thor::Actions
-
-    desc "new", "Creates a new Rails project using the Varvet template"
-    def new(name)
-      system("rails new #{name} --database=mysql --template=#{template_path}")
-    end
-
-    desc "update", "Updates the Rails project with the latest Varvet template"
-    def update
-      system("bundle exec rake rails:template LOCATION=#{template_path}")
-    end
-
-    desc "deploy", "Deploys the application to the specified environment"
-    def deploy(env)
-      system("bundle exec cap #{env} deploy")
-    end
-
-  end
-end
