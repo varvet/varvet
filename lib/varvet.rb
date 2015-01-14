e3e9384523897465d19f9fc73985fe4d808cbc26
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-01-14 10:43:05 +0100

    Update varvet.rb

--- lib/varvet.rb
+++ lib/varvet.rb
@@ -5,7 +5,7 @@ module Varvet
   module Rails
     class Railtie < ::Rails::Railtie
       config.before_initialize do
-        if Rails.env.production?
+        if ::Rails.env.production?
           ::Rails.logger = Logger.new(STDOUT)
           ::Rails.logger.level = Logger.const_get("INFO")
         end
